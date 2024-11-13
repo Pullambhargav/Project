7957fd4d9f05ff76dfb8eb52ec1de4ea8ee0cf06
Pullambhargav <pullambhargavgoud@gmail.com>
2024-11-14 02:12:09 +0530

    updated

--- new_start/settings.py
+++ new_start/settings.py
@@ -105,10 +105,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Adjust if your static folder is located differently
-]
+STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",  # Adjust if your static folder is located differently
+# ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
