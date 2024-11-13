e848977eb5252055be2eaa25c3c24caf75cb7a51
Pullambhargav <pullambhargavgoud@gmail.com>
2024-11-13 22:06:25 +0530

    Add Render domain to ALLOWED_HOSTS

--- new_start/settings.py
+++ new_start/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-c&wl$s3)6ni8aa%=h@1xir12vk#6)u(0%-3!aen$=lgga3#rh+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'project-ic4c.onrender.com']
 
 
 # Application definition
